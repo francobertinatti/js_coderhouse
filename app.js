7e499cedcbb843e1c7c42179baa27f397fa8eff7
francobertinatti <franbertinatti@gmail.com>
2022-11-07 22:57:36 -0300

    Entrega 2

--- app.js
+++ app.js
@@ -1,111 +1,194 @@
-//Pin acceso
-let PIN = "1234";
-let USUARIO = "franco";
+// Variables
+const baseDeDatos = [
+  {
+    id: 1,
+    nombre: "Pechuga",
+    precio: 100,
+    costo: 50,
+    imagen: "assets/burger.jpg",
+  },
+  {
+    id: 2,
+    nombre: "Filet",
+    precio: 200,
+    costo: 50,
+    imagen: "assets/burger.jpg",
+  },
+  {
+    id: 3,
+    nombre: "Sorrentinos",
+    precio: 180,
+    costo: 50,
+    imagen: "assets/burger.jpg",
+  },
+  {
+    id: 4,
+    nombre: "Correa",
+    precio: 300,
+    costo: 50,
+    imagen: "assets/burger.jpg",
+  },
+  {
+    id: 5,
+    nombre: "Manzana",
+    precio: 300,
+    costo: 50,
+    imagen: "assets/burger.jpg",
+  },
+  {
+    id: 6,
+    nombre: "Jamon",
+    precio: 300,
+    costo: 50,
+    imagen: "assets/burger.jpg",
+  },
+  {
+    id: 7,
+    nombre: "Jamon",
+    precio: 300,
+    costo: 50,
+    imagen: "assets/burger.jpg",
+  },
+];
 
-function login() {
-// Declaramos la variable ingresar como "false" para cancelar el ingreso
-    let ingresar = false;
-// Le damos 2 intentos al usuario, comenzando desde 0, en cuanta regresiva reduciendo de a -1 iniciando en 3
-    for (let i = 2; i >= 0; i--) {
-        let userUSUARIO = prompt("Ingresa tu usuario");
-        let userPIN = prompt("Ingresa tu clave");
-    if (userPIN === PIN && userUSUARIO === USUARIO) {
-        alert("Bienvenido/a Ingreso exitoso");
-        ingresar = true;
-        break;
-    } else {
-        alert("Error te quedan " + i + " intentos");
-    }
-}
-    return ingresar;
-}
+let carrito = [];
+const moneda = "$";
+const DOMitems = document.querySelector("#items");
+const DOMcarrito = document.querySelector("#carrito");
+const DOMtotal = document.querySelector("#total");
+const DOMbotonVaciar = document.querySelector("#boton-vaciar");
 
-// function validacionCalidad(stock,devolucion) { verificar!
-//     // let controlCalidad;
+// Funciones
 
-//     // if (!confirm("¿Todas las unidades pasaron el control de calidad?")) {
-//     //     controlCalidad =parseInt(prompt("¿Cuantas unidades pasaron el control de calidad?"));
-//     //         stock = stock + controlCalidad;
-        
-//     // } else {
-//     //     stock = stock + devolucion;
-        
-//     // }
-//     // alert("Mercaderia ingresada con exito, tu nuevo stock es de " + stock);
-// }
-
-
-
-
-
-let logueo = login();
-console.log(logueo);
-
-if (logueo) {
-let stock = 0;
-
-let opcion = prompt(
-    "Elegí una opción: \n1- Stock Actual \n2 - Ventas (▼ Stock) \n3 - Compras (▲ Stock) \n4 - Devolucion Mercaderia (▲ Stock) \n Presioná ESC para cerrar sesion."
-);
-
-while (opcion != "ESC" && opcion != "esc") {
-    switch (opcion) {
-    case "1":
-        alert("Tu stock actual es " + stock);
-        break;
-    case "2":
-        let ventas = parseInt(prompt("Ingresa las unidades vendidas"));
-        // si las ventas ej 2 es menor al stock actual ej 1 entonces "stock insuficente, realiza una orden de compra" - si ventas es 1 y stock es 1 vendiste 1 unidad tu stock 
-        if (ventas <= stock && ventas>0) {
-        stock = stock - ventas;
-        alert("Vendiste " + ventas + "unidades, tu stock actual es  " + stock);
-        } else {
-        alert("No puede realizar la operacion");
-        }
-        break;
-
-    case "3":
-        let compras = parseInt(prompt("Igresa las unidades compradas"));
-        stock = stock + compras;
-        alert("Mercaderia ingresada con exito, tu nuevo stock es de " + stock);
-        break;
-
-        case "4":
-            let devolucion = parseInt(prompt("Ingresa las unidades devueltas"));
-            let controlCalidad;
-            if (!confirm("¿Todas pasaron el control?")) {
-                controlCalidad = parseInt(
-                    prompt("¿Cuántas pasaron el control de calidad?"),
-                );
-                while (controlCalidad > devolucion) {
-                    alert("Operacion invalida, verifica las unidades ingresadas nuevamente.");
-                    controlCalidad = parseInt(
-                        prompt("¿Cuántas pasaron el control de calidad?"),
-                    );
-                }
-                stock = stock + controlCalidad;
-            } else {
-                stock = stock + devolucion;
-            }
-            alert("Mercaderia ingresada con exito, tu nuevo stock es de " + stock);
-            break;
-
-    // case "5": verificar!
-    //     let devolucion = parseInt(prompt("Ingresa las unidades devueltas"));
-    //     validacionCalidad(stock,devolucion);
-    //     break;
-
-
-    default:
-        alert("Opción no válida");
-        break;
-    }
-    opcion = prompt(
-    "¿Quieres registrar otra operacion? \n Elegí una opción: \n1- Stock Actual \n2 - Ventas (▼ Stock) \n3 - Compras (▲ Stock) \n4 - Devolucion Mercaderia (▲ Stock) \nPresioná ESC para cerrar sesion."
-    );
-}
-} else {
-alert("Bloqueamos preventivamente tu cuenta, comunicate con el administrador");
+function renderizarProductos() {
+  //Se ahdieren todos los productos al HTML (NO AL CARRITO)
+  baseDeDatos.forEach((info) => {
+    // Estructura
+    const miNodo = document.createElement("div");
+    miNodo.classList.add("card", "col-sm-2", "g-4", "m-3");
+    // Body
+    const miNodoCardBody = document.createElement("div");
+    miNodoCardBody.classList.add("card-body");
+    // Titulo
+    const miNodoTitle = document.createElement("h5");
+    miNodoTitle.classList.add("card-title");
+    miNodoTitle.textContent = info.nombre;
+    // Imagen
+    const miNodoImagen = document.createElement("img");
+    miNodoImagen.classList.add("img-fluid");
+    miNodoImagen.setAttribute("src", info.imagen);
+    // Precio
+    const miNodoPrecio = document.createElement("p");
+    miNodoPrecio.classList.add("card-text");
+    miNodoPrecio.textContent = `${info.precio}${moneda}`;
+    // Boton
+    const miNodoBoton = document.createElement("button");
+    miNodoBoton.classList.add("btn", "btn-outline-warning");
+    miNodoBoton.textContent = "Agregar";
+    miNodoBoton.setAttribute("marcador", info.id);
+    miNodoBoton.addEventListener("click", anyadirProductoAlCarrito);
+    // Insertamos
+    miNodoCardBody.appendChild(miNodoImagen);
+    miNodoCardBody.appendChild(miNodoTitle);
+    miNodoCardBody.appendChild(miNodoPrecio);
+    miNodoCardBody.appendChild(miNodoBoton);
+    miNodo.appendChild(miNodoCardBody);
+    DOMitems.appendChild(miNodo);
+  });
 }
 
-alert("Cerraste sesion con exito");
+/**
+ * Evento para añadir un producto al carrito de la compra
+ */
+function anyadirProductoAlCarrito(evento) {
+  // Anyadimos el Nodo a nuestro carrito
+  carrito.push(evento.target.getAttribute("marcador"));
+  // Actualizamos el carrito
+  renderizarCarrito();
+}
+
+/**
+ * Dibuja todos los productos guardados en el carrito
+ */
+function renderizarCarrito() {
+  // Vaciamos todo el html
+  DOMcarrito.textContent = "";
+  // Quitamos los duplicados
+  const carritoSinDuplicados = [...new Set(carrito)];
+  // Generamos los Nodos a partir de carrito
+  carritoSinDuplicados.forEach((item) => {
+    // Obtenemos el item que necesitamos de la variable base de datos
+    const miItem = baseDeDatos.filter((itemBaseDatos) => {
+      // ¿Coincide las id? Solo puede existir un caso
+      return itemBaseDatos.id === parseInt(item);
+    });
+    // Cuenta el número de veces que se repite el producto
+    const numeroUnidadesItem = carrito.reduce((total, itemId) => {
+      // ¿Coincide las id? Incremento el contador, en caso contrario no mantengo
+      return itemId === item ? (total += 1) : total;
+    }, 0);
+    // Creamos el nodo del item del carrito
+    const miNodo = document.createElement("li");
+    miNodo.classList.add("list-group-item", "text-right", "mx-2");
+    miNodo.textContent = `${numeroUnidadesItem} x ${miItem[0].nombre} - ${miItem[0].precio}${moneda}`;
+    // Boton de borrar
+    const miBoton = document.createElement("button");
+    miBoton.classList.add("btn", "btn-danger", "mx-5");
+    miBoton.textContent = "X";
+    miBoton.style.marginLeft = "1rem";
+    miBoton.dataset.item = item;
+    miBoton.addEventListener("click", borrarItemCarrito);
+    // Mezclamos nodos
+    miNodo.appendChild(miBoton);
+    DOMcarrito.appendChild(miNodo);
+  });
+  // Renderizamos el precio total en el HTML
+  DOMtotal.textContent = calcularTotal();
+}
+
+/**
+ * Evento para borrar un elemento del carrito
+ */
+function borrarItemCarrito(evento) {
+  // Obtenemos el producto ID que hay en el boton pulsado
+  const id = evento.target.dataset.item;
+  // Borramos todos los productos
+  carrito = carrito.filter((carritoId) => {
+    return carritoId !== id;
+  });
+  // volvemos a renderizar
+  renderizarCarrito();
+}
+
+/**
+ * Calcula el precio total teniendo en cuenta los productos repetidos
+ */
+function calcularTotal() {
+  // Recorremos el array del carrito
+  return carrito
+    .reduce((total, item) => {
+      // De cada elemento obtenemos su precio
+      const miItem = baseDeDatos.filter((itemBaseDatos) => {
+        return itemBaseDatos.id === parseInt(item);
+      });
+      // Los sumamos al total
+      return total + miItem[0].precio;
+    }, 0)
+    .toFixed(2);
+}
+
+/**
+ * Varia el carrito y vuelve a dibujarlo
+ */
+function vaciarCarrito() {
+  // Limpiamos los productos guardados
+  carrito = [];
+  // Renderizamos los cambios
+  renderizarCarrito();
+}
+
+// Eventos
+DOMbotonVaciar.addEventListener("click", vaciarCarrito);
+// Inicio
+renderizarProductos();
+renderizarCarrito();
